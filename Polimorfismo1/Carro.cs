e745d57ddfba199c1d8f4df689a93cd1e0030c12
Abraham V. R <sneak_89@hotmail.com>
2024-03-19 09:39:52 -0600

    Agregar archivos de proyecto.

---
+++ Polimorfismo1/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo1
+{
+    internal class Carro
+    {
+        //Atributos
+        protected string color { get; set; }
+    }
+}
---
+++ Polimorfismo1/Carroceria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo1
+{
+    internal class Carroceria : Carro
+    {
+        //Atributos
+        private double bas { get; set; }
+        private double altura { get; set; }
+
+        //Constructor
+        public Carroceria(string c, double b, double a) 
+        { 
+            color = c;
+            this.bas = b;
+            this.altura = a;
+        }
+
+        //Metodo
+        public string MostrarArea()
+        {
+            string datos;
+            double area1;
+            area1 = bas * altura;
+            datos = "El color de la carroceria es: " + color + Environment.NewLine;
+            datos += "El area de la carroceria es: " + area1 + Environment.NewLine;
+            return datos; 
+        }
+    }
+}
